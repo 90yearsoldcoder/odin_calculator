90f031bf044ef3e53566173991227ffd067336e5
90yearsoldcoder <minty@bu.edu>
2023-11-10 20:54:33 -0500

    JS function done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,23 @@
+let disply_string = "";
+let cur_string = "";
+
+//add listenners
+/*
+document.querySelectorAll(".btn").forEach((element) => {
+  element.addEventListener("click", clickButton);
+});
+*/
+//Number input listenner
+document.querySelectorAll(".number").forEach((element) => {
+  element.addEventListener("click", addNumberListener);
+});
+document.querySelectorAll(".self_operation").forEach((element) => {
+  element.addEventListener("click", selfOperationListener);
+});
+document.querySelectorAll(".operation").forEach((element) => {
+  element.addEventListener("click", OperationLisener);
+});
+
 function add(a, b) {
   return a + b;
 }
@@ -15,7 +35,7 @@ function divide(a, b) {
 }
 
 function str2operation(str) {
-  let match = str.match(/(-?\d+(?:\.\d+)?)\s*([\+\-\*\/])\s*(-?\d+(?:\.\d+)?)/);
+  let match = str.match(/(-?\d+(?:\.\d+)?)\s*([\+\-\×\÷])\s*(-?\d+(?:\.\d+)?)/);
   let a, b, op;
   if (match) {
     a = parseFloat(match[1]);
@@ -24,9 +44,8 @@ function str2operation(str) {
   } else {
     return "Invalid!";
   }
-  console.log(a);
-  console.log(b);
-  console.log(op);
+  //console.log(a, b, op);
+  return [a, b, op];
 }
 
 function calculate(a, b, op) {
@@ -35,14 +54,106 @@ function calculate(a, b, op) {
       return add(a, b);
     case "-":
       return substract(a, b);
-    case "*":
+    case "×":
       return multiply(a, b);
-    case "/":
+    case "÷":
       return divide(a, b);
     default:
       return "Error: Invalid operation";
   }
 }
 
-st = " 10.1 * 21.05";
-str2operation(st);
+function clickButton(e) {
+  console.log(e.target.value);
+}
+
+function updateCurString() {
+  document.getElementById("curNumber").textContent = cur_string;
+}
+function updateDisplayString() {
+  document.getElementById("displayNumber").textContent = disply_string;
+}
+
+function addNumberListener(e) {
+  if (cur_string.length > 8) return;
+  cur_string += e.target.value;
+  //console.log(cur_string);
+  updateCurString();
+}
+
+//handle +/-, AC, DEL, .
+function selfOperationListener(e) {
+  let op_type = e.target.value;
+  let newStr;
+  //console.log(op_type);
+  switch (op_type) {
+    case "clear":
+      newStr = "";
+      disply_string = "";
+      break;
+    case "del":
+      newStr = cur_string.substring(0, cur_string.length - 1);
+      break;
+    case "plus/minus":
+      if (cur_string.startsWith("-"))
+        newStr = cur_string.substring(1, cur_string.length);
+      else newStr = "-" + cur_string;
+      break;
+    case ".":
+      if (cur_string.includes(".")) newStr = cur_string;
+      else newStr = cur_string + ".";
+      break;
+    default:
+      console.log("error, Invaild operation");
+      newStr = cur_string;
+  }
+  cur_string = newStr;
+  updateCurString();
+  updateDisplayString();
+}
+
+function equalOperation() {
+  let full_str = disply_string + cur_string;
+  let split_str = str2operation(full_str);
+  if (split_str == "Invalid!") return "Invalid!";
+
+  //calculate, if a,b,op are availble
+  let ans = calculate(split_str[0], split_str[1], split_str[2]);
+
+  disply_string = full_str;
+  cur_string = "" + ans;
+}
+
+//listener, handle +, -, ×, /
+function OperationLisener(e) {
+  flag = equalOperation();
+  let op_type = e.target.value;
+
+  switch (op_type) {
+    case "+":
+      disply_string = cur_string + "+";
+      updateDisplayString();
+      break;
+    case "-":
+      disply_string = cur_string + "-";
+      updateDisplayString();
+      break;
+    case "×":
+      disply_string = cur_string + "×";
+      updateDisplayString();
+      break;
+    case "÷":
+      disply_string = cur_string + "÷";
+      updateDisplayString();
+      break;
+    case "=":
+      if (flag == "Invalid!") return;
+      disply_string = disply_string + "=";
+      updateDisplayString();
+      disply_string = "";
+      updateCurString();
+      return;
+  }
+  updateCurString();
+  cur_string = "";
+}
